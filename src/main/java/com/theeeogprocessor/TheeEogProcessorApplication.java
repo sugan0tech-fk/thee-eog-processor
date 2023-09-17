dacdd6f8b98eaad1f6f8d5eaa15731091a0fe5a7
sugavanesh <71312159+sugan0tech@users.noreply.github.com>
2023-09-17 22:50:04 +0530

    initial commit

---
+++ src/main/java/com/theeeogprocessor/TheeEogProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.theeeogprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheeEogProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheeEogProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theeeogprocessor/TheeEogProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theeeogprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheeEogProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
