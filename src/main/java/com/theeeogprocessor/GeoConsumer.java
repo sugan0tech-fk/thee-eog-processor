2a76f8f6385e087ec5fe0bf0c8a42b388c772458
sugan0tech-fk <sugavanesh.m@fourkites.com>
2023-09-18 19:45:39 +0530

    added lombok and loggers

--- src/main/java/com/theeeogprocessor/GeoConsumer.java
+++ src/main/java/com/theeeogprocessor/GeoConsumer.java
@@ -1,12 +1,15 @@
 package com.theeeogprocessor;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class GeoConsumer {
     @KafkaListener(topics = "geo", groupId = "geo-consumer")
     public void consume(String message){
-        System.out.println(message);
+        log.info("received value is :" + message);
     }
 }
--- src/main/java/com/theeeogprocessor/ProducerController.java
+++ src/main/java/com/theeeogprocessor/ProducerController.java
@@ -1,5 +1,6 @@
 package com.theeeogprocessor;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("")
+@AllArgsConstructor
 public class ProducerController {
     private GeoProducer geoProducer;
 
-    public ProducerController(GeoProducer geoProducer){
-        this.geoProducer = geoProducer;
-    }
     @PostMapping("/add_geo_data")
     public void addGeoData(@RequestBody String data){
         geoProducer.sendGeoData(data);
